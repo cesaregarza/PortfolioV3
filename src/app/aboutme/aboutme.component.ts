9c16795e458e4d622449ece87e213a00d3f34aff
Unknown <cesar.garza.826@gmail.com>
2018-09-16 19:13:33 -0500

    Canvas DPI

--- src/app/aboutme/aboutme.component.ts
+++ src/app/aboutme/aboutme.component.ts
@@ -19,8 +19,25 @@ export class AboutmeComponent implements OnInit {
   ngOnInit() {
     let ctx: CanvasRenderingContext2D =
     this.canvasRef.nativeElement.getContext('2d');
+    let dpi = window.devicePixelRatio;
+    let canvasEle = document.getElementById('aCanvas');
+
     
     this.canvasRef.nativeElement.style.maxHeight = "100px";
+    let style_height = +getComputedStyle(canvasEle).getPropertyValue("height").slice(0, -2);
+    let style_width = +getComputedStyle(canvasEle).getPropertyValue("width").slice(0, -2);
+
+    let canvasHeight = style_height * dpi;
+    let canvasWidth = style_width * dpi;
+    
+    canvasEle.setAttribute('height', "" + canvasHeight);
+    canvasEle.setAttribute('width', "" + canvasWidth);
+
+    ctx.font = "" + (canvasHeight * 0.8) + "px Arial"
+    ctx.fillStyle = "white";
+    ctx.fillText("About", 0, 0.8 * canvasHeight);
+    
+
   }
   
 
